43f881b6730d83affc6a58d9e010ce6546459c2b
Abdullah Tariq <tariqhina053@gmail.com>
2026-08-30 12:26:00 +0500

    Add about page and navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello Abdullah</h1>
-      <p>Main Next.js seekh raha hoon 🚀</p>
+      
+    
+      <Navbar />
     </div>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>My Website</h2>
+
+      <div>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+      </div>
+    </nav>
+  );
+}
